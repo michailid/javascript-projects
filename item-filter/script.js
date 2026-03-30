9543ad38a906115a678dd8476cfff3d7117c8c99
michailid <michailidoumaria224@gmail.com>
2026-03-30 20:48:59 +0300

    minor change

--- item-filter/script.js
+++ item-filter/script.js
@@ -8,7 +8,7 @@ const items = document.querySelectorAll('#item-list li');
 searchBox.addEventListener('input', () => {
     const query = searchBox.value.toLowerCase().trim();
     items.forEach(item => {
-        const match = item.textContent.toLowerCase().includes(query);
-        item.style.display = match ? '' : 'none';
+        const matches = item.textContent.toLowerCase().includes(query);
+        item.style.display = matches ? '' : 'none';
   });
 });
