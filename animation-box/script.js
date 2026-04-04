37776750fa27d9dbb4fb436930ab530878061343
michailid <michailidoumaria224@gmail.com>
2026-04-04 23:22:26 +0300

    animation-box: style & box motion

---
+++ animation-box/script.js
@@ -0,0 +1,14 @@
+function moveBox() {
+    let box = document.getElementById("animated-box");
+    let motion = setInterval(frame, 10);
+    let pos = 0;
+    function frame() {
+        if (pos == 450) {
+            clearInterval(motion);
+        } else {
+            pos++;
+            box.style.top = pos + "px";
+            box.style.right = pos + "px"
+        }
+    }
+} 
